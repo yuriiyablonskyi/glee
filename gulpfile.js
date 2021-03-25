903ffb4c3991799484e792d6ee409c45a00baa54
yuriiyablonskyi <yuriiyablonskyi.i@gmail.com>
2021-03-25 20:48:15 +0100

    settig sprite

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,30 @@
 const { src, dest, watch, parallel, series } = require('gulp');
-
-const svgSprite = require("gulp-svg-sprites");
 const scss = require('gulp-sass');
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 const del = require('del');
+const svgSprite = require("gulp-svg-sprites");
+// var gulp = require('gulp');
 const browserSync = require('browser-sync').create();
-// const svgSprite    = require('gulp-svg-sprite');
-
-// const svgSprites = () => {
-//   return src('app/svg-sprite/*.svg')
-//   .pipe(svgSprite({
-//     mode: {
-//       stack: {
-//         sprite: "../images/sprite.svg"
-//       }
-//     }
-//   }))
-//   .pipe(dest('app/'))
-// }
 
 function browsersync() {
   browserSync.init({
     server: {
       baseDir: 'app/'
     },
-    notofy: false
+    notify: false
   })
 }
+
 
 function styles() {
   return src('app/scss/style.scss')
     .pipe(scss({ outputStyle: 'compressed' }))
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({
-      overrideBrowserslist: ['last 10 version'],
+      overrideBrowserslist: ['last 10 versions'],
       grid: true
     }))
     .pipe(dest('app/css'))
@@ -76,7 +64,7 @@ function build() {
   return src([
     'app/**/*.html',
     'app/css/style.min.css',
-    'app/js/main.min.js'
+    'app/js/main.min.js',
   ], { base: 'app' })
     .pipe(dest('dist'))
 }
@@ -85,79 +73,46 @@ function cleanDist() {
   return del('dist')
 }
 
+// function svgSprites() {
+//   return src('app/images/svg/*.svg')
+//     .pipe(svgSprite({
+//       mode: {
+//         stack: {
+//           sprite: '../sprite.svg'
+//         }
+//       }
+//     }))
+//     .pipe(dest('app/images'));
+// };
+
+function svgSprites() {
+  return src('./app/images/svg/**.svg')
+  .pipe(svgSprite({
+    mode: {
+      stack: {
+        sprite: "../sprite.svg"
+      }
+    }
+  }))
+  .pipe(dest('./app/images'))
+}
+
+
+
 function watching() {
   watch(['app/scss/**/*.scss'], styles);
   watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
   watch(['app/**/*.html']).on('change', browserSync.reload);
 }
 
-function svgSprites() {
-  return src('app/images/*.svg')
-  .pipe(svgSprite({
-    mode: {
-      stack: {
-        sprite: '../sprite.svg'
-      }
-    }
-  }))
-  .pipe(dest('app/images'));
-};
-
 exports.styles = styles;
-exports.svgSprites = svgSprites;
 exports.scripts = scripts;
 exports.browsersync = browsersync;
 exports.watching = watching;
 exports.images = images;
 exports.cleanDist = cleanDist;
+exports.build = series(cleanDist, images, build);
+exports.svgSprites = svgSprites;
 
 exports.default = parallel(styles, scripts, svgSprites, browsersync, watching);
-exports.build = series(cleanDist, images, build);
-watch('./src/img/**.svg').svgSprites;
 
-// exports.default = parallel(styles, scripts, svgSprites, browsersync, watching);
-// var gulp = require('gulp'),
-//   svgSprite = require('gulp-svg-sprite'),
-
-//   // More complex configuration example
-//   config = {
-//     shape: {
-//       dimension: { // Set maximum dimensions
-//         maxWidth: 32,
-//         maxHeight: 32
-//       },
-//       spacing: { // Add padding
-//         padding: 10
-//       },
-//       dest: 'out/intermediate-svg' // Keep the intermediate files
-//     },
-//     mode: {
-//       view: { // Activate the «view» mode
-//         bust: false,
-//         render: {
-//           scss: true // Activate Sass output (with default options)
-//         }
-//       },
-//       symbol: true // Activate the «symbol» mode
-//     }
-//   };
-
-// gulp.src('app/svg-sprite/*.svg', { cwd: 'path/to/assets' })
-//   .pipe(svgSprite(config))
-//   .pipe(gulp.dest('out'));
-
-// // SVG Config
-// var config = {
-//   mode: {
-//     symbol: { // symbol mode to build the SVG
-//       render: {
-//         css: false, // CSS output option for icon sizing
-//         scss: false // SCSS output option for icon sizing
-//       },
-//       dest: 'sprite', // destination folder
-//       prefix: '.svg--%s', // BEM-style prefix if styles rendered
-//       sprite: 'sprite.svg', //generated sprite name
-//       example: true // Build a sample page, please!
-//     }
-//   }
-// };
